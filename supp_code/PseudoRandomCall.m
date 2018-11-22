e7ab57c5d993bde6566cca63bf0976aa9b750236
thao.dang@univ-grenoble-alpes.fr <thao.dang@univ-grenoble-alpes.fr>
2018-11-22 15:11:08 +0100

    updated PseudoRandomCall to be separate from Classification

---
+++ supp_code/PseudoRandomCall.m
@@ -0,0 +1,59 @@
+function StatFalsObj=PseudoRandomCall(StatFalsObj, CBS, phi, time_lim, nb_samples, nb_hits)
+ % NewSys = CallPseudo(Sys, xlog_vec, xbest_vec)
+ % function: run pseudo-random sampling
+ %
+ % Inputs/Outputs:
+ % Sys: CoverageBreachSet object to which new samples are added.
+ % Outputs:
+ % NewSys: New CoverageBreachSet object after adding all the new points.
+ 
+ 
+ %global Out
+ 
+
+
+CBS.SetSnapToGrid(true);
+    
+max_sim = inf; %maximal number of simulations
+
+%tic
+
+%w_rob=0.5; % weight for robustness
+
+% since this is pseudo-random sampling only, without classification
+% we call this function with init_sim = max_sim 
+% init_sim=nb sim required for classification
+
+
+
+ip = CBS.VaryingParamList;
+param_names = CBS.P.ParamList(1,ip);
+param_ranges = [CBS.LowerLeftCorner(),CBS.UpperRightCorner()];  
+StatFalsObj.nominal_ranges = param_ranges;
+            
+[StatFalsObj,list,falsified,nb,falsi_point] = StatFalsObj.PseudoRandSamp(CBS,param_names,...
+  param_ranges,phi,nb_samples,time_lim,nb_hits);
+
+if nb>0
+   StatFalsObj.new_samples=list;
+   
+   [newminrob, min_id] = min(list.vals)
+   
+   if (~isempty(StatFalsObj.lower_bounds)) 
+     if ( StatFalsObj.lower_bounds.vals(1,:) > newminrob )
+       StatFalsObj.lower_bounds.vals(1,:) = newminrob;
+       StatFalsObj.lower_bounds.pts(1,:) = list.pts(min_id,:);
+     end
+   else
+     StatFalsObj.lower_bounds.vals(1,:) = newminrob;
+     StatFalsObj.lower_bounds.pts(1,:) = list.pts(min_id,:); 
+   end
+end
+
+
+
+
+%StatFalsify([],CBS,phi,w_rob,max_sim,max_sim,time_lim);
+
+% time = toc;
+% fprintf(1, 'Computation time = %f seconds \n',time);
--- supp_code/initSolver.m
+++ supp_code/initSolver.m
@@ -1,4 +1,4 @@
-function x0 = initSolver(CBS,nbsamples,solver_index,Xlog,Xbest,strategy_id,winlen)
+function x0 = initSolver(CBS,nbsamples,solver_index,num_solvers,SolverInfo,strategy_id,winlen)
  % initSolver(xlog_vec, xbest_vec)
  % function: pick an initial point for solvers 
  %
@@ -17,9 +17,9 @@ xbest_vec= [];
     switch strategy_id
         case 0 %init from xlogPR  
                 
-                xlogPR_vec = Xlog.xlogPR;
-                
-                nb_pt_xlogPR =size(xlogPR_vec,2)
+                %xlogPR_vec = Xlog.xlogPR;
+                xlogPR_vec = SolverInfo(1).Xlog;
+                nb_pt_xlogPR =size(xlogPR_vec,2);
                 if (nbsamples>nb_pt_xlogPR)
                     nbsamples = nb_pt_xlogPR;
                 end
@@ -48,17 +48,27 @@ xbest_vec= [];
                 end %end while
                 
         case 1 %init from xlog 
-                if (solver_index == 1 ) 
-                    xlog_vec = [xlog_vec, Xlog.xlogSA, Xlog.xlogGNM];
-                end
-                if (solver_index == 2 ) 
-                    xlog_vec = [xlog_vec, Xlog.xlogCMAES, Xlog.xlogGNM];
-                end
-                if (solver_index == 3 ) 
-                    xlog_vec = [xlog_vec, Xlog.xlogCMAES, Xlog.xlogSA];
+%                 if (solver_index == 1 ) 
+%                     %xlog_vec = [xlog_vec, Xlog.xlogSA, Xlog.xlogGNM];
+%                     xlog_vec = [xlog_vec, Xlog(2,:), Xlog(3,:)];
+%                 end
+%                 if (solver_index == 2 ) 
+%                     %xlog_vec = [xlog_vec, Xlog.xlogCMAES, Xlog.xlogGNM];
+%                     xlog_vec = [xlog_vec, Xlog(1,:), Xlog(3,:)];
+%                 end
+%                 if (solver_index == 3 ) 
+%                     %xlog_vec = [xlog_vec, Xlog.xlogCMAES, Xlog.xlogSA];
+%                     xlog_vec = [xlog_vec, Xlog(1,:), Xlog(2,:)];
+%                 end
+                
+                %%% collecting xlog but not from the current solver
+                for (ii=1:1:num_solvers)
+                    if (~(ii==solver_index)) 
+                      xlog_vec = [xlog_vec, SolverInfo(ii).Xlog];
+                    end
                 end
                 
-                nb_pt_xlog=size(xlog_vec,2)
+                nb_pt_xlog=size(xlog_vec,2);
                 if (nbsamples>nb_pt_xlog)
                     nbsamples = nb_pt_xlog;
                 end
@@ -103,23 +113,44 @@ xbest_vec= [];
 %                     nbsamples = nb_pt_xbest;
 %                 end
                 
-                nbsamples
-                strategy_id
+                %nbsamples
+                %strategy_id
                 
-                solversdone= [ 0 0 0 ];
+                solversdone= zeros(num_solvers);  %[ 0 0 0 ];
                 nbsolversdone = 0;
-                if (size(Xbest.xbestCMAES,2)>0) 
-                    solversdone(1) = 1;
-                    nbsolversdone = nbsolversdone + 1;
+%                 if (size(Xbest.xbestCMAES,2)>0) 
+%                     solversdone(1) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+%                 if (size(Xbest.xbestSA,2)>0) 
+%                     solversdone(2) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+%                 if (size(Xbest.xbestGNM,2)>0) 
+%                     solversdone(3) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+                
+%                 if (size(Xbest(2,:),2)>0) 
+%                     solversdone(1) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+%                 if (size(Xbest(3,:),2)>0) 
+%                     solversdone(2) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+%                 if (size(Xbest(4,:),2)>0) 
+%                     solversdone(3) = 1;
+%                     nbsolversdone = nbsolversdone + 1;
+%                 end
+
+                for ii = 1:1:(num_solvers)
+                    if (size(SolverInfo(ii).Xbest,2)>0) 
+                      solversdone(1) = 1;
+                      nbsolversdone = nbsolversdone + 1;
+                    end
                 end
-                if (size(Xbest.xbestSA,2)>0) 
-                    solversdone(2) = 1;
-                    nbsolversdone = nbsolversdone + 1;
-                end
-                if (size(Xbest.xbestGNM,2)>0) 
-                    solversdone(3) = 1;
-                    nbsolversdone = nbsolversdone + 1;
-                end
+                
                 
                 itrial=1;
                 while (itrial<=nbsamples && nbsolversdone>0)
@@ -133,41 +164,43 @@ xbest_vec= [];
 %                     end
                     
                     if (solversdone(rand_solverid) >0)
-                        switch rand_solverid 
-                            case 1 
-                                nb_pt_xbest=size(Xbest.xbestCMAES,2);
+%                         switch rand_solverid 
+%                             case 1 
+%                                 nb_pt_xbest=size(Xbest(rand_solverid+1,:),2);
                                 if nb_pt_xbest>0
                                     rand_id = randi(nb_pt_xbest);
                                     if (rand_id<0 || rand_id>nb_pt_xbest)
                                         error('Error in rand_id_xbest');
                                     end
-                                    x0i = Xbest.xbestCMAES(:,rand_id);
-                                    Xbest.xbestCMAES(:,rand_id) = [];
+                                    xbest_vec = SolverInfo(rand_solverid+1).Xbest;
+                                    x0i = xbest_vec(:,rand_id);
+                                    xbest_vec(:,rand_id) = [];
+                                    SolverInfo(rand_solverid+1).Xbest = xbest_vec;
                                 end
 
-                            case 2  
-                                nb_pt_xbest=size(Xbest.xbestSA,2);
-                                if nb_pt_xbest>0
-                                    rand_id = randi(nb_pt_xbest);
-                                    if (rand_id<0 || rand_id>nb_pt_xbest)
-                                        error('Error in rand_id_xbest');
-                                    end
-                                    x0i = Xbest.xbestSA(:,rand_id);
-                                    Xbest.xbestSA(:,rand_id) = [];
-                                end
-                            case 3
-                                nb_pt_xbest=size(Xbest.xbestGNM,2);
-                                if nb_pt_xbest>0
-                                    rand_id = randi(nb_pt_xbest);
-                                    if (rand_id<0 || rand_id>nb_pt_xbest)
-                                        error('Error in rand_id_xbest');
-                                    end
-                                    x0i = Xbest.xbestGNM(:,rand_id);
-                                    Xbest.xbestGNM(:,rand_id) = [];
-                                end
-                            otherwise
-                                error('rand_id')
-                        end
+%                             case 2  
+%                                 nb_pt_xbest=size(Xbest.xbestSA,2);
+%                                 if nb_pt_xbest>0
+%                                     rand_id = randi(nb_pt_xbest);
+%                                     if (rand_id<0 || rand_id>nb_pt_xbest)
+%                                         error('Error in rand_id_xbest');
+%                                     end
+%                                     x0i = Xbest.xbestSA(:,rand_id);
+%                                     Xbest.xbestSA(:,rand_id) = [];
+%                                 end
+%                             case 3
+%                                 nb_pt_xbest=size(Xbest.xbestGNM,2);
+%                                 if nb_pt_xbest>0
+%                                     rand_id = randi(nb_pt_xbest);
+%                                     if (rand_id<0 || rand_id>nb_pt_xbest)
+%                                         error('Error in rand_id_xbest');
+%                                     end
+%                                     x0i = Xbest.xbestGNM(:,rand_id);
+%                                     Xbest.xbestGNM(:,rand_id) = [];
+%                                 end
+%                             otherwise
+%                                 error('rand_id')
+%                         end
 
                         if nb_pt_xbest>0
     %                         rand_id
@@ -187,10 +220,12 @@ xbest_vec= [];
                 
 
         otherwise
-            error('error in strategy_id')
+            error('error in reinit_strategy')
     
     
     end %end of switch
+    
+    
 %     disp('Initial point :');
 %     x0
 
