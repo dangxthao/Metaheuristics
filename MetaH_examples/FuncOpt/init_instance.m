fc0e19198c5a088f09b8de1793f2038c786dc646
thao.dang@univ-grenoble-alpes.fr <thao.dang@univ-grenoble-alpes.fr>
2019-05-21 15:20:23 +0200

    typos

--- MetaH_examples/FuncOpt/init_instance.m
+++ MetaH_examples/FuncOpt/init_instance.m
@@ -7,7 +7,7 @@ B_ripple.dt_default =1;
 B_ripple.SetParamRanges({'x1', 'x2'}, [0 1]);
 
 R = BreachRequirement('y[t]>0');
-Pb_ripple = FalsificationProblem(Bripple,R);
+Pb_ripple = FalsificationProblem(B_ripple,R);
 Pb_ripple.StopAtFalse=0;
 
 B_holder = BreachSignalGen(fun_signal_gen('holder'));
