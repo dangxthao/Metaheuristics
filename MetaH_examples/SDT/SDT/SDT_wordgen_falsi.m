d7019d4576448547f558206f680b46a611efae31
thao.dang@univ-grenoble-alpes.fr <thao.dang@univ-grenoble-alpes.fr>
2019-10-18 17:44:55 +0200

    changes in SDT_falsi_wordgen_optim

--- MetaH_examples/SDT/SDT/SDT_wordgen_falsi.m
+++ MetaH_examples/SDT/SDT/SDT_wordgen_falsi.m
@@ -27,11 +27,37 @@ sdt_template = sdt_template(1:5*num_evt);
 pg.set_template_in(sdt_template);
 pg.template_in = regexprep(pg.template_in,'\[(\w+)\]', '\[_\]'); % erase letters 
 
+pg
+
+% TA_file: '../../../wordgen/hscc20.json'
+%       num_events: 50
+%      wordgen_exe: '../../../wordgen/wordgen'
+%             poly: 5
+%      template_in: '%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]%g[_]'
+%     template_out: '%g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g'
+%          val_map: [5×1 containers.Map]
+%           p0_out: [101×1 double]
+%           params: {1×53 cell}
+%           domain: [1×53 BreachDomain]
+%       domain_out: [1×101 BreachDomain]
+%       params_out: {1×101 cell}
+%               p0: [53×1 double]
+
+pg.params
+% pg.domain
+% 
+% pg.params_out
+% pg.domain_out
+
+input('Press ENTER');
 
 %%
 
 time = linspace(0,1e-5, 10000);
 S = BreachSignalGen(sg);
+
+S
+input('Press ENTER');
 
 %%
 B = BP2.copy();
@@ -44,8 +70,7 @@ B.SetParam({'time_scale'}, [1e-8 : 1e-8 : 1e-7]);
 B.SetParam({'beta'}, [-3 : 1 : 0], 'combine');
 B.SetParam({'alpha'}, [.1 1 10], 'combine');
 
-
-
+input('Press ENTER');
 
 
 %%
