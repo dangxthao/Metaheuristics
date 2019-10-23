7309c3a7c581bc3411af71bd8b71d2b2db18b443
Alexandre Donze <alex@decyphir.com>
2019-10-23 16:23:31 +0200

    wordgen falsi

--- MetaH_examples/SDT/SDT/SDT_wordgen_falsi.m
+++ MetaH_examples/SDT/SDT/SDT_wordgen_falsi.m
@@ -5,16 +5,12 @@ warning('OFF', 'ALL')
 
 %% Setup DeltaSigma model
 init_SDT
-phi = STL_Formula('notsaturation', 'alw(OutSat[t]<2)');
-R = BreachRequirement(phi); 
 
 %% Wordgen generator
-
 addpath('../../../wordgen');
 num_evt = 60;
 sg = var_cp_signal_gen('In1',num_evt+2, 'linear');
 
-%% 
 pg = TA_param_gen('In1','../../../wordgen/hscc20.json', num_evt);
 pg.wordgen_exe = '../../../wordgen/wordgen';
 sdt_template = '%g[b]%g[c]%g[d]';
@@ -26,56 +22,58 @@ end
 sdt_template = sdt_template(1:5*num_evt);
 pg.set_template_in(sdt_template);
 pg.template_in = regexprep(pg.template_in,'\[(\w+)\]', '\[_\]'); % erase letters 
+params_evt = pg.params(1:end-3);
+ranges = repmat([0,1], numel(params_evt),1);
 
 S = BreachSignalGen(sg);
 
-%% Testing against timescale 
-time = linspace(0,2e-6, 10000);
-B = BP2.copy();
-B.SetInputGen(S);
-B.SetParamGen(pg);
-B.SetParam('time_scale', [1e-8 : 5e-11 : 2e-8]);
-B.SetTime(time);
-B.Sim();
-B.PlotSignals();
-R.Eval(B);
-F = BreachSamplesPlot(R); 
 
-%% Testing against events
-Be = BP2.copy();
-Be.SetInputGen(S);
-Be.SetParamGen(pg);
-Be.SetParam('time_scale', 1.1e-8);
-Be.SetTime(time);
+%% Time and requirements
+time = linspace(0,1.5e-6, 10000);
+ts = .9e-8; % time scale
+STL_ReadFile('SDT_req.stl');
 
-params = pg.params(1:end-3);
-ranges = repmat([0,1], numel(params),1);
-
-Be.SetParamRanges(params, ranges);
-Be.SampleDomain(params, 100); % random (pseudo not quasi) sampling of 10
-R = BreachRequirement(phi); 
-R.Eval(Be);
-F = BreachSamplesPlot(R); 
-F.set_y_axis('notsaturation');
 
 %% Falsification Problem
-Bpb = BP2.copy();
-Bpb.SetInputGen(S);
-Bpb.SetParamGen(pg);
-Bpb.SetParam('time_scale', 1.2e-8);
-Bpb.SetTime(time);
-Rpb = BreachRequirement(phi); 
-pb = FalsificationProblem(Bpb, Rpb, params, ranges);
+
+Bwg = BP2.copy();
+Bwg.SetInputGen(S);
+Bwg.SetParamGen(pg);
+Bwg.SetParam('time_scale', ts);
+Bwg.SetTime(time);
+
+Rwg = BreachRequirement(notsat, {}, per); 
+pbwg = FalsificationProblem(Bwg, Rwg, params_evt, ranges);
 
 %%
-pb.max_obj_eval=1000;
-pb.solver_options.num_corners = 0; 
-pb.solver_options.num_quasi_rand_samples = 50;
-pb.solve();
-Rpb= pb.GetLog();
+pbwg.max_obj_eval=1000;
+pbwg.solver_options.num_corners = 0; 
+pbwg.solver_options.num_quasi_rand_samples = 50;
+pbwg.solve();
+Rpb= pbwg.GetLog();
 
 %%
-Fpb = BreachSamplesPlot(Rpb); 
-Fpb.set_y_axis('notsaturation');
+Fwg = BreachSamplesPlot(Rpb); 
+Fwg.set_y_axis('notsaturation');
+
+%% Testing without wordgen (free dt)
+Bn = BP2.copy();
+Bn.SetInputGen(S);
+Bn.SetParam('time_scale', ts);
+Bn.SetTime(time);
+Rn = BreachRequirement(notsat, {}, per);
+params_dt = Bn.expand_param_name('In1_dt.');
+params_u = Bn.expand_param_name('In1_u.');
+ranges_dt = repmat([0 4*ts], numel(params_dt), 1);
+p0_u = Bwg.GetParam(params_u);
+Bn.SetParamRanges(params_dt, ranges_dt)
+Bn.SetParam(params_u, p0_u)
+
+pbn = FalsificationProblem(Bn, Rn, params_dt, ranges_dt);
+pbn.max_obj_eval=10;
+pbn.solver_options.num_corners = 0; 
+pbn.solver_options.num_quasi_rand_samples = 50;
+pbn.solve();
+Rpb= pbn.GetLog();
 
 
