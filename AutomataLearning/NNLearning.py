6371d936aa99c55466eaa7deda22e94717fac0a7
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2020-06-03 13:58:05 +0200

    Implemented NNLearning to learn the neural net by Irini's algorithm

--- AutomataLearning/NNLearning.py
+++ AutomataLearning/NNLearning.py
@@ -1,12 +1,42 @@
 import matlab.engine
 import sys
+from learning import Float_Range, NonAdequateTeacher_MM_Float
+from learning import SymbPACLearner_MM_Float as SymbLearner_MM_Float
 
-if 'MATLAB_4242' not in matlab.engine.find_matlab():
-    print("MATLAB session not found. Terminating...")
-    sys.exit(-1)
+class NNTeacher:
+    def __init__(self):
+        if 'MATLAB_4242' not in matlab.engine.find_matlab():
+            print("MATLAB session not found. Terminating...")
+            sys.exit(-1)
 
-print("Connecting to MATLAB session...")
-eng = matlab.engine.connect_matlab('MATLAB_4242')
-print("Connected to MATLAB session successfully!")
+        print("Connecting to MATLAB session...")
+        self.eng = matlab.engine.connect_matlab('MATLAB_4242')
+        print("Connected to MATLAB session successfully!")
+        self.concrete_alphabet = Float_Range(-10, 10)
+    def compute(self, word):
+        if word == '':
+            return str(0)
+        else:
+            word = word.split()
+            inp = [float(c) for c in word]
+            if len(inp) == 1:
+                new = [inp[0]]
+                new.extend(inp)
+                inp = new
+            inp = matlab.double(inp)
+            out = self.eng.NN_MembershipQuery(inp)
+            if out > 0:
+                return str(+1)
+            else:
+                return str(-1)
 
 
+T = NonAdequateTeacher_MM_Float(NNTeacher())
+
+L = SymbLearner_MM_Float(T,e =.1, d = .1,  print_on = True, file_name_prefix = 'L'+str(1))
+
+L.run()
+
+print("Success!")
+
+L.hypothesis[-1].open_graph('H')
