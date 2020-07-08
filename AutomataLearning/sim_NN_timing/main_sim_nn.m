2f09f9b6d3cf3714d3ea7e9563d5fc0518105688
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2020-07-08 09:32:55 +0200

    Changes by Nikos to run neural net with variable sized input

--- AutomataLearning/sim_NN_timing/main_sim_nn.m
+++ AutomataLearning/sim_NN_timing/main_sim_nn.m
@@ -26,6 +26,8 @@ load(mat_file)
 % create net
 % gensim(net)
 
+%% simulate with delays
+
 
 %% find info
 
@@ -52,7 +54,7 @@ for i=1:no_iterations
     elseif i==1 && example==2
         in1=[0.04;0.048;0.46;0.82;0.7;0.55;0.2;0.64;0.045;0.72];
     elseif i>1 && example==2
-        in1=rand(1,10)'*10;   
+        in1=rand(1,10)'*10;
     end
     out1=sim(net,in1);
     t1=toc;
@@ -63,3 +65,31 @@ for i=1:no_iterations
     
     fprintf('Test %i -- Matlab: %.5f sec vs. Simulink %.5f sec.\n\n',i,t1,t2)
 end
+
+%% simulate delays
+
+load('net_object.mat')
+tic
+% in_net=[0.59;0.47;0.29;0.28;0.17;0.58];
+in_rand=rand(10000,1)'*10;
+in1=in_rand(1:(end-2));
+in2=in_rand(2:(end-1));
+in3=in_rand(3:(end));
+
+in_rand=rand(10000,1)'*10;
+in4=in_rand(1:(end-2));
+in5=in_rand(2:(end-1));
+in6=in_rand(3:(end));
+in_all=[in1;in2;in3;in4;in5;in6];
+out1=sim(net,in_all);
+t1=toc
+
+%% bad solution not working
+model_name='NN_2019';
+model_name='NN_2019_breach';
+
+
+tic;
+%In1=in1;
+sim(model_name);
+t2=toc
